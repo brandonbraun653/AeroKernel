2de1c89c6a120f07aea0728fa54d1b098d5ca674
Brandon Braun <brandonbraun653@gmail.com>
2019-05-11 13:49:25 -0500

    Added several functions and a few tests

--- AeroKernel/parameter.cpp
+++ AeroKernel/parameter.cpp
@@ -14,15 +14,112 @@
 
 namespace AeroKernel::Parameter
 {
+  /*------------------------------------------------
+  Compile Time Checks
+  ------------------------------------------------*/
+  static constexpr size_t ParamCtrlBlkSize = sizeof( ParamCtrlBlk );
+  static_assert( ( sizeof( ParamCtrlBlk ) * sizeof( uint32_t ) ) % 32 == 0, "Parameter control block is not 32-bit aligned" );
+
+  static_assert( static_cast<uint8_t>( MemoryLocation::MAX_MEMORY_LOCATIONS ) == 8, "Incorrect supported memory locations" );
+
+  /*------------------------------------------------
+  Constants
+  ------------------------------------------------*/
+  static constexpr size_t param_buffer_cutoff = 3; /* Limit how close we can get to filling the parameter manager*/
+
+
   Manager::Manager()
   {
-
   }
 
   Manager::~Manager()
   {
-
   }
 
+  bool Manager::init( const size_t numElements )
+  {
+    params.clear();
+    params.resize( numElements );
+    memoryDriver.fill( nullptr );
 
-}  // namespace AeroKernel
+    initialized = true;
+
+    return true;
+  }
+
+  bool Manager::registerParameter( const std::string_view &key, const ParamCtrlBlk &controlBlock )
+  {
+    bool result = true;
+
+    if ( !initialized )
+    {
+      result = false;
+    }
+    else if ( ( params.max_size() - params.size() ) <= param_buffer_cutoff )
+    {
+      result = false;
+    }
+    else
+    {
+      /*------------------------------------------------
+      If the key does not exist in the map, it will be added. Otherwise
+      the existing key will be accessed and the value updated.
+      ------------------------------------------------*/
+      params[ key ] = controlBlock;
+    }
+
+    return result;
+  }
+
+  bool Manager::isRegistered( const std::string_view &key )
+  {
+    return params.contains( key );
+  }
+
+  bool Manager::read( const std::string_view &key, void *const param, const size_t size )
+  {
+    return false;
+  }
+
+  bool Manager::write( const std::string_view &key, const void *const param, const size_t size )
+  {
+    return false;
+  }
+
+  bool Manager::update( const std::string_view &key )
+  {
+    return false;
+  }
+
+  bool Manager::remove( const std::string_view &key )
+  {
+    return false;
+  }
+
+  bool Manager::registerMemoryDriver( const MemoryLocation storage, Chimera::Modules::Memory::Device_sPtr &driver )
+  {
+    bool result = true;
+
+    if ( !initialized || ( storage >= MemoryLocation::MAX_MEMORY_LOCATIONS ) )
+    {
+      result = false;
+    }
+    else
+    {
+      memoryDriver[ static_cast<uint8_t>( storage ) ] = driver;    
+    }
+
+    return result;
+  }
+
+  bool Manager::registerMemorySpecs( const MemoryLocation storage, const Chimera::Modules::Memory::Descriptor &specs )
+  {
+    return false;
+  }
+
+  const AeroKernel::Parameter::ParamCtrlBlk &Manager::getControlBlock( const std::string_view &key )
+  {
+    return params[ key ];
+  }
+
+}  // namespace AeroKernel::Parameter
--- AeroKernel/parameter.hpp
+++ AeroKernel/parameter.hpp
@@ -13,32 +13,76 @@
 #define AERO_KERNEL_PARAMETER_MANAGER_HPP
 
 #include <cstdint>
+#include <string>
+#include <memory>
+
+#include <sparsepp/spp.h>
+
+/* Chimera Includes */
+#include <Chimera/modules/memory/device.hpp>
 
 namespace AeroKernel::Parameter
 {
+
+  enum class MemoryLocation : uint8_t
+  {
+    INTERNAL_SRAM = 0,
+    INTERNAL_FLASH,
+    EXTERNAL_FLASH0,
+    EXTERNAL_FLASH1,
+    EXTERNAL_FLASH2,
+    EXTERNAL_SRAM0,
+    EXTERNAL_SRAM1,
+    EXTERNAL_SRAM2,
+
+    MAX_MEMORY_LOCATIONS
+  };
+
+  struct ParamCtrlBlk
+  {
+    size_t size;
+    uint32_t address;
+    uint32_t config;
+    bool ( *update )( void *const, const size_t );
+  };
+
+  using ParamCtrlBlk_sPtr = std::shared_ptr<ParamCtrlBlk>;
+  using ParamCtrlBlk_uPtr = std::unique_ptr<ParamCtrlBlk>;
+
+
   class Manager
   {
   public:
-
     Manager();
     ~Manager();
 
-    void registerParameter();
+    bool init( const size_t numElements );
 
-    void isRegistered();
+    bool registerParameter( const std::string_view &key, const ParamCtrlBlk &controlBlock );
 
-    void read();
-    
-    void write();
+    bool isRegistered( const std::string_view &key );
 
-    void update();
+    bool read( const std::string_view &key, void *const param, const size_t size );
 
-    void remove();
+    bool write( const std::string_view &key, const void *const param, const size_t size );
 
-    // Driver needs to be lockable...is it? Maybe it should be a compile time check...
-    void registerMemoryDriver(/*MemLocation, SharedPtr to Chimera MemDevice*/); 
+    bool update( const std::string_view &key );
 
+    bool remove( const std::string_view &key );
+
+    bool registerMemoryDriver( const MemoryLocation storage, Chimera::Modules::Memory::Device_sPtr &driver );
+
+    bool registerMemorySpecs( const MemoryLocation storage, const Chimera::Modules::Memory::Descriptor &specs );
+
+    const ParamCtrlBlk &getControlBlock( const std::string_view &key );
+
+  protected:
+    bool initialized = false;
+    spp::sparse_hash_map<std::string_view, ParamCtrlBlk> params;
+    std::array<Chimera::Modules::Memory::Device_sPtr, static_cast<size_t>( MemoryLocation::MAX_MEMORY_LOCATIONS )> memoryDriver;
   };
-}
+
+
+}  // namespace AeroKernel::Parameter
 
 #endif /* !AERO_KERNEL_PARAMETER_MANAGER_HPP */
